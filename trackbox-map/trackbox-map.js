bd62aeffcb0aa5853b5baa3107ebb7d1f42415ab
tattii <yuta.tatti@gmail.com>
2016-10-06 17:39:05 +0900

    init interface

---
+++ trackbox-map/trackbox-map.js
@@ -0,0 +1,22 @@
+/*
+ * TrackboxMap - trackbox map class based on Google Maps
+ *
+ * ref. Overlay map types
+ * https://developers.google.com/maps/documentation/javascript/examples/maptype-overlay
+ *
+ */
+
+/** @constructor */
+function TrackboxMap(def) {
+	this.def = def;
+}
+
+TrackboxMap.prototype.addTo = function(map) {
+
+};
+
+
+TrackboxMap.prototype.getTile = function(coord, zoom, owner) {
+
+};
+
