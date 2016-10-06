465c9d2c132520b8a68724022474c6f1acbc531e
tattii <yuta.tatti@gmail.com>
2016-10-06 20:06:27 +0900

    implement overlay map type

--- trackbox-map/trackbox-map.js
+++ trackbox-map/trackbox-map.js
@@ -8,15 +8,61 @@
 
 /** @constructor */
 function TrackboxMap(def) {
-	this.def = def;
+	this.tileSize = new google.maps.Size(256, 256);
+	this._def = def;
+
+	this._tileBounds = new google.maps.LatLngBounds(
+		new google.maps.LatLng(def.bounds[0][0], def.bounds[0][1]),
+		new google.maps.LatLng(def.bounds[1][0], def.bounds[1][1]));
+	console.log(this);
 }
 
 TrackboxMap.prototype.addTo = function(map) {
+	this.map = map;
 
+	map.fitBounds(this._tileBounds);
+
+	map.overlayMapTypes.insertAt(0, this);
 };
 
 
 TrackboxMap.prototype.getTile = function(coord, zoom, owner) {
+	var tile = owner.createElement('img');
 
+	var tileBounds = this._tileCoordsToBounds(coord, zoom);
+
+	if (tileBounds.intersects(this._tileBounds)){
+		if (zoom >= this._def.zoom.min && zoom <= this._def.zoom.max){
+			tile.alt = '';
+			tile.src = this._getTileUrl(coord, zoom);
+			tile.style.width = this.tileSize.width + 'px';
+			tile.style.height = this.tileSize.height + 'px';
+		}
+	}
+	
+	return tile;
+};
+
+
+TrackboxMap.prototype._getTileUrl = function(coord, zoom) {
+	var y = (1 << zoom) - coord.y - 1;
+	return this._def.url + '/' + zoom + '/' + coord.x + '/' + y + '.png';
+};
+
+TrackboxMap.prototype._tileCoordsToBounds = function(coord, zoom) {
+	var proj = this.map.getProjection();
+	var scale = Math.pow(2, zoom);
+
+	var p1 = new google.maps.Point(
+		(coord.x + 1)* this.tileSize.width / scale,
+		coord.y * this.tileSize.height / scale);
+	var p2 = new google.maps.Point(
+		coord.x * this.tileSize.width / scale,
+		(coord.y + 1) * this.tileSize.height / scale);
+	
+	var ne = proj.fromPointToLatLng(p1);
+	var sw = proj.fromPointToLatLng(p2);
+
+	return new google.maps.LatLngBounds(sw, ne);
 };
 
