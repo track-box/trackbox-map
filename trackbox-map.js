63076acdf327dd19938d4e40c1c513887e59e037
tattii <yuta.tatti@gmail.com>
2016-10-12 14:08:08 +0900

    support retina #4

--- trackbox-map.js
+++ trackbox-map.js
@@ -18,6 +18,8 @@ function TrackboxMap(def) {
 	this._tileBounds = new google.maps.LatLngBounds(
 		new google.maps.LatLng(def.bounds[0][0], def.bounds[0][1]),
 		new google.maps.LatLng(def.bounds[1][0], def.bounds[1][1]));
+
+	this._retina = window.devicePixelRatio >= 2;
 }
 
 TrackboxMap.prototype.addTo = function(map) {
@@ -32,22 +34,58 @@ TrackboxMap.prototype.addTo = function(map) {
 
 
 TrackboxMap.prototype.getTile = function(coord, zoom, owner) {
-	var tile = owner.createElement('img');
-	tile.alt = '';
-
 	var tileBounds = this._tileCoordsToBounds(coord, zoom);
 
 	if (tileBounds.intersects(this._tileBounds)){
 		if (zoom >= this._def.zoom.min && zoom <= this._def.zoom.max){
-			tile.src = this._getTileUrl(coord, zoom);
-			tile.style.width = this.tileSize.width + 'px';
-			tile.style.height = this.tileSize.height + 'px';
+
+			if (this._retina && zoom < this._def.zoom.max){
+				var tile = owner.createElement('div');
+				tile.style.width = this.tileSize.width + 'px';
+				tile.style.height = this.tileSize.height + 'px';
+
+				this._createRetinaTile(tile, coord, zoom + 1, 0, 0);
+				this._createRetinaTile(tile, coord, zoom + 1, 0, 1);
+				this._createRetinaTile(tile, coord, zoom + 1, 1, 0);
+				this._createRetinaTile(tile, coord, zoom + 1, 1, 1);
+
+				return tile;				
+
+			}else{
+				var tile = owner.createElement('img');
+				tile.alt = '';
+
+				tile.src = this._getTileUrl(coord, zoom);
+				tile.style.width = this.tileSize.width + 'px';
+				tile.style.height = this.tileSize.height + 'px';
+
+				return tile;
+			}
 		}
 	}
 	
+	var tile = owner.createElement('img');
+	tile.alt = '';
 	return tile;
 };
 
+
+TrackboxMap.prototype._createRetinaTile = function(tile, coord, zoom, px, py) {
+	var coord1 = { x: coord.x * 2 + px, y: coord.y * 2 + py };
+	var tileBounds = this._tileCoordsToBounds(coord1, zoom);
+
+	if (tileBounds.intersects(this._tileBounds)){
+		var tile1 = document.createElement('img');
+		tile1.src = this._getTileUrl(coord1, zoom);
+		tile1.style.width = (this.tileSize.width / 2) + 'px';
+		tile1.style.height = (this.tileSize.height / 2) + 'px';
+		tile1.style.position = 'absolute';
+		tile1.style.top = (this.tileSize.width / 2 * py) + 'px';
+		tile1.style.left = (this.tileSize.height / 2 * px) + 'px';
+
+		tile.appendChild(tile1);
+	}
+};
 
 TrackboxMap.prototype._getTileUrl = function(coord, zoom) {
 	var y = (1 << zoom) - coord.y - 1;
